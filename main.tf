3998f12db2823c0f2c62f272bbf418230d3f6441
devops-learner-phani <109220162+devops-learner-phani@users.noreply.github.com>
2026-03-29 18:33:34 +0530

    terraform learning

--- main.tf
+++ main.tf
@@ -39,7 +39,8 @@ resource "null_resource" "ansible" {
       password = "DevOps321"
     }
     inline = [
-      "sudo labauto ansible",
+      "sudo dnf install python3.13-pip.noarch -y",
+      "sudo pip3.13 install ansible",
       "ansible-pull -i localhost, -U https://github.com/phanikolluri/wmp-ansible-v4.git main.yml -e env=dev -e COMPONENT=${each.key}"
     ]
   }
